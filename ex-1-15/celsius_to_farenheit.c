57973fdf739794a5fa2d579840ead06df704b69c
sourya <souryadeepsen80@gmail.com>
2021-09-12 21:16:56 -0400

    initial commit for exercise 1-15 k&r solution

---
+++ ex-1-15/celsius_to_farenheit.c
@@ -0,0 +1,32 @@
+/*
+k&r exercise 1-15: Rewrite the temperature conversion program of section 1.2 to use a function for conversion
+*/
+
+#include <stdio.h>
+
+#define LOWER 0			/*lower limit of table. this is a symbolic constant in C, does not need to be terminated by ;*/
+#define UPPER 300
+#define STEP 20
+
+float celsius_to_farenheit(int fahr); //this is the function prototype as required in C
+
+void main(){
+	
+	float fahr, celsius;
+	int c;
+
+	//c = getchar();
+	//printf("%s\n",EOF);
+
+	fahr=UPPER;
+	printf("Farenheit\tCelsius\n");
+	while(fahr >= LOWER){
+		celsius = celsius_to_farenheit(fahr);
+		printf("%3.0f\t\t%6.1f\n",fahr,celsius);
+		fahr = fahr - STEP;
+	}
+}
+
+float celsius_to_farenheit(int fahr){ //function definition
+	return (5.0/9.0)*(fahr-32);
+}
